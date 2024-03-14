3ffbc227d2cdc54229bfa01320d08c3ccb7c869f
unknown <ikohistory5@gmail.com>
2024-03-14 21:52:29 +0900

    v.0.0 pokemon game

---
+++ src/PokemonGame.java
@@ -0,0 +1,41 @@
+class Pokemon{
+  private String name;
+  private int hp;
+
+  // crtl + insert
+  public Pokemon() {
+      System.out.println("default constructor");
+  }
+
+  public Pokemon(String name, int hp) {
+      this.name = name;
+      this.hp = hp;
+      System.out.println("parameter constructor");
+  }
+
+  public String getName() {
+      return name;
+  }
+
+  public void setName(String name) {
+      this.name = name;
+  }
+
+  public int getHp() {
+      return hp;
+  }
+
+  public void setHp(int hp) {
+      this.hp = hp;
+  }
+}
+
+public class PokemonGame {
+    public static void main(String[] args) {
+        Pokemon p1 = new Pokemon();
+        Pokemon p2 = new Pokemon("Squritle", 70);
+        p1.setHp(50);
+        p1.setName("Pikachu");
+        System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
+    }
+}
--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -1,32 +1,34 @@
 import java.util.Scanner;
 
 public class PrimeNumber {
+
+    private static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        } else {
+            for (int i = 2; i * i <= n; i = i + 1) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         boolean isPrimeNumber = true;
         Scanner scanner = new Scanner(System.in);
         int numberStart = scanner.nextInt();
         int numberEnd = scanner.nextInt();
 
-        if(numberStart > numberEnd){
+        if (numberStart > numberEnd) {
             int temp = numberStart;
             numberStart = numberEnd;
             numberEnd = temp;
         }
 
-            for (int k = numberStart; k <= numberEnd; k++) {
-            isPrimeNumber = true;
-
-            if (k < 2) {
-                isPrimeNumber = false;
-            } else {
-                for (int i = 2; i * i <= k; i = i + 1) {
-                    if (k % i == 0) {
-                        isPrimeNumber = false; // remove add operation
-                        break;
-                    }
-                }
-                if(isPrimeNumber) System.out.print(k + " ");
-            }
+        for (int k = numberStart; k <= numberEnd; k++) {
+            if (isPrimeNumber(k)) System.out.print(k + " ");
         }
     }
 }
