1d269e3cbbc6629cc616d0eac8eaa552c27f2402
modu2I <162572603+modu2I@users.noreply.github.com>
2024-03-14 20:24:31 +0900

    v.0.6 prime number

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -4,16 +4,13 @@ public class PrimeNumber {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        //int count = 0;
         boolean isPrimeNumber = true;
 
         if(number < 2){
-            //count = 1;
             isPrimeNumber = false;
         }else {
             for (int i = 2; i*i <= number; i = i + 1) {
                 if (number % i == 0) {
-                    //count = count + 1;
                     isPrimeNumber = false; // remove add operation
                     break;
                 }
