62b8a5bdf18788bb053f9fd54e638ca5625e46c5
unknown <ikohistory5@gmail.com>
2024-03-21 20:13:08 +0900

    v.0.3 pokemon game

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -1,40 +1,4 @@
-class Pokemon{
-  private String name;
-  private int hp;
-
-  // crtl + insert
-  public Pokemon() {
-      this.name = "nameless";
-      this.hp = 10;
-      System.out.println("default constructor");
-  }
-
-  public Pokemon(String n, int h) {
-//      정상작동, 가동선 떨어짐.
-//      name = n;
-//      hp = h;
-      this.name = name;
-      this.hp = hp;
-      System.out.println("parameter constructor");
-  }
-
-  public String getName() {
-      return name;
-  }
-
-  public void setName(String name) {
-      this.name = name;
-  }
-
-  public int getHp() {
-      return hp;
-  }
-
-  public void setHp(int hp) {
-      this.hp = hp;
-  }
-}
-
+import pokemons.Pokemon;
 public class PokemonGame {
     public static void main(String[] args) {
         Pokemon p1 = new Pokemon();
