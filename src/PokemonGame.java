fc1eb5de3ce7905c44190bffbf95bf3a52142c1e
unknown <ikohistory5@gmail.com>
2024-03-28 20:19:49 +0900

    v.1.1 pokemon game

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -1,12 +1,19 @@
+import pokemons.Charizard;
 import pokemons.Pikachu;
 import pokemons.Pokemon;
 import pokemons.Squirtle;
 
 public class PokemonGame {
     public static void main(String[] args) {
+        Charizard c1 = new Charizard();
+        c1.setName("Charizard");
+        c1.setHp(150);
         Pikachu p1 = new Pikachu("Pikachu", 50);
         Squirtle s2 = new Squirtle("Squirtle", 70);
         s2.attack(p1);
         p1.attack(s2);
+        c1.attack();
+        c1.attack(p1);
+        c1.attack(s2);
     }
 }
---
+++ src/pokemons/Charizard.java
@@ -0,0 +1,15 @@
+package pokemons;
+
+public class Charizard extends Pokemon{
+
+
+    @Override
+    public void attack() {
+        System.out.println("Wide area fire attack");
+    }
+
+    @Override
+    public void attack(Pokemon targetPokemon) {
+        System.out.println(this.getName() + " fires a firethrower attack at " + targetPokemon.getName());
+    }
+}
