9c24a3e8f060bca33595c91fa4b1bfe0032b78f0
unknown <ikohistory5@gmail.com>
2024-03-28 21:43:28 +0900

    v.1.9 pokemon game

--- src/pokemons/NoFly.java
+++ src/pokemons/NoFly.java
@@ -3,6 +3,6 @@ package pokemons;
 public class NoFly implements Flyable{
     @Override
     public void fly() {
-        System.out.println("Pokemon can't fly");
+        System.out.println(" can't fly");
     }
 }
--- src/pokemons/Rocket.java
+++ src/pokemons/Rocket.java
@@ -3,6 +3,6 @@ package pokemons;
 public class Rocket implements Flyable{
     @Override
     public void fly() {
-        System.out.println("Pokémon flies through the sky with a rocket on its back.");
+        System.out.println(" flies through the sky with a rocket on its back.");
     }
 }
