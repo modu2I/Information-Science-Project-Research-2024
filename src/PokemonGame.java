d4e49174f35860f7956902044df6282370418681
unknown <ikohistory5@gmail.com>
2024-04-04 19:28:54 +0900

    v2.0 pokemon game

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -1,26 +1,24 @@
 import pokemons.*;
 
+import java.util.Scanner;
+
 public class PokemonGame {
     public static void main(String[] args) {
-        Charizard c1 = new Charizard("Charlizard", 150); // Aggregation
-//        c1.setFlyable(new Wings());
-        c1.performFly();
-        Pikachu p1 = new Pikachu("Pikachu", 50);
-//        p1.setFlyable(new NoFly());
-        p1.performFly();
-        Rocket rocket = new Rocket();
-        p1.setFlyable(rocket);
-        p1.performFly();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose your pokemon.\n1) Pikachu 2) Squirtle 3) Charizard : ");
+        int select = scanner.nextInt();
 
-//        c1.setName("Charizard");
-//        c1.setHp(150);
-//        c1.fly();
-//        Pikachu p1 = new Pikachu("Pikachu", 50);
-//        Squirtle s2 = new Squirtle("Squirtle", 70);
-//        s2.attack(p1);
-//        p1.attack(s2);
-//        c1.attack();
-//        c1.attack(p1);
-//        c1.attack(s2);
+        Pokemon playerPokemon;
+        if(select == 1)
+            playerPokemon = new Pikachu();
+        else if (select == 2)
+            playerPokemon = new Squirtle();
+        else if (select == 3)
+            playerPokemon = new Charizard();
+
+        while(true){
+            System.out.println("pass");
+            break;
+        }
     }
 }
